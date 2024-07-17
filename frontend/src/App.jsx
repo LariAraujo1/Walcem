1d124167bd0f2da5f9983b4b81bd5c7d0b56e34a
Ana Paula <ana.ppviana@senacsp.edu.br>
2024-07-17 16:28:03 -0300

    Atualização de estruturas de pastas 17/07

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/routes/Navigation.js';
-import Blog from './components/blogPage/Blog.js';
+import Blog from './pages/Blog/b.js';
+import css from './components'
 import privacy from './components/politicsPage/privacy-policy.js';
-import css from './css/privacy.css';
+
 import { CSSProperties } from 'react';
 
 function App() {
