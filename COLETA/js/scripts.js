e7ee2dd3f21e9cb72641db573c001667710dbabf
Cris <162187334+CrisSenac@users.noreply.github.com>
2024-07-27 21:26:46 -0300

    final js cadastro empresa

---
+++ COLETA/js/scripts.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    const inputs = form.querySelectorAll("input");
+
+    inputs.forEach((input, index) => {
+        input.addEventListener("keydown", function(event) {
+            if (event.key === "Tab") {
+                event.preventDefault();
+                const nextInput = inputs[index + 1];
+                if (nextInput) {
+                    nextInput.focus();
+                } else {
+                    inputs[0].focus(); // Loop back to the first input if it's the last input
+                }
+            }
+        });
+    });
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Requisição enviada com sucesso!");
+    });
+});
+
+//  icones
+document.addEventListener('DOMContentLoaded', function() {
+    const icons = document.querySelectorAll('.icon');
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', function() {
+            this.classList.toggle('selected');
+        });
+    });
+});
