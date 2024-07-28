894cda55ac4f822eae9e2268c7905cf50edc0187
Larissa Araujo <162187647+LariAraujo1@users.noreply.github.com>
2024-07-28 21:55:30 +0000

    cadastro

---
+++ cadastro/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CadastroPage from './components/CadastroPage';
+
+function App() {
+    return (
+        <div className="App">
+            <CadastroPage />
+        </div>
+    );
+}
+
+export default App;
